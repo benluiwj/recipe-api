19e90b530399935e56b99f46dc762e221046e5cc
benluiwj <wenjie.lui@gmail.com>
2023-02-07 18:48:41 +0800

    feat: update schema

---
+++ db/migrate/20230207104445_add_username_to_recipe.rb
@@ -0,0 +1,5 @@
+class AddUsernameToRecipe < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :recipes, :user, column: :user_name
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_06_134239) do
+ActiveRecord::Schema.define(version: 2023_02_07_104445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,7 @@ ActiveRecord::Schema.define(version: 2023_02_06_134239) do
     t.string "procedure"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "recipes_id"
-    t.index ["recipes_id"], name: "index_procedures_on_recipes_id"
+    t.integer "recipes_id"
   end
 
   create_table "recipes", force: :cascade do |t|
