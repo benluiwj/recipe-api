3ae05bece0c3db3bfeed8c360a54e5b19e0f787c
benluiwj <wenjie.lui@gmail.com>
2023-02-07 19:16:25 +0800

    feat: add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding..."
+
+User.create(user_name: 'test', email: 'test@gmail.com', password: '1234', name: 'test')
