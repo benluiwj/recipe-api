1d892386939d955acf79bbcaaa8a4675b003ddb7
benluiwj <wenjie.lui@gmail.com>
2023-02-14 14:33:31 +0800

    feat: add private ingredient methods

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -13,4 +13,17 @@ class IngredientsController < ApplicationController
 
 	def destroy
 	end
+
+	private
+	def ingredient_params
+		params.permit(:name, :amount, :units)
+	end
+
+	def all_ingredients
+		ingredients = Ingredient.references(params[:recipe_id])
+	end
+
+	def ingredient
+		@ingredient = Ingredient.references(params[:recipe_id]).find(params[:id])
+	end
 end
