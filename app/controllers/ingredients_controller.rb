caa75fe8b84752fdb87213d61e77fa42cb785088
benluiwj <wenjie.lui@gmail.com>
2023-02-14 14:33:57 +0800

    feat: add index and show routes

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,8 +1,18 @@
 class IngredientsController < ApplicationController
 	def index
+		if all_ingredients
+			render json: all_ingredients, status: :ok
+		else
+			render json: {message: "No recipe found"}, status: :not_found
+		end
 	end
 
 	def show
+		if ingredient
+			render json: ingredient, status: :ok
+		else
+			render json: ingredient.errors, status: :not_found
+		end
 	end
 
 	def create
